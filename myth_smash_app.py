56c7c29b9cbfd27ef0bab94219592cea13bd0b73
lbwilson12-dot <lbwilson12@gmail.com>
2026-02-25 17:29:29 -0800

    Update myth_smash_app.py

--- myth_smash_app.py
+++ myth_smash_app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Myth Smash", page_icon="💙")
 # -----------------------------
 NAMI_BLUE = "#FFFFFF"
 LIGHT_BLUE = "#0c499c "
-
+White = "FFFFFF"
 st.markdown(
     f"""
     <style>
@@ -54,7 +54,18 @@ QUESTION_BANK = [
         "question": "Mental illness is rare.",
         "answer": "Myth",
         "explanation": "1 in 5 adults experience mental illness each year."
-    }
+    },
+    {
+        "question": "A person with a mental health condition is usually more likely to be violent than anyone else in our population",
+        "answer": "Myth",
+        "explanation": "people with mental health conditions are not, as a group, more likely to be violent than the general population. In fact, they are over 10 times more likely to be victims of violent crime than perpetrators.",
+    },
+    {
+        "question": "Women are 70% more likely than men to suffer from a Depressive Disorder",
+        "answer": "Fact",
+        "explanation": "This disparity is driven by a complex interaction of hormonal fluctuations, genetic vulnerability, and higher societal stressors.",
+    
+    },
 ]
 
 # -----------------------------
@@ -152,12 +163,17 @@ else:
         "https://forms.gle/rkfjprteYAynvR6e6" 
     )
 
-    st.markdown("---")
+    # SUBMIT SCORE & STAY CONNECTED
+    st.link_button(
+        "Submit Score & Stay Connected",
+        f"https://docs.google.com/forms/d/e/1FAIpQLSdk_BFkywBWfGdR5M0UsEzVGgPRy7SlMBjAoQRbXpSPl0P7pA/viewform?usp=pp_url&entry.1110337880={st.session_state.score}"
+    )
 
-    if st.button("Play Again"):
+if st.button("Play Again"):
         st.session_state.questions = random.sample(QUESTION_BANK, len(QUESTION_BANK))
         st.session_state.index = 0
         st.session_state.score = 0
         st.session_state.show_feedback = False
         st.session_state.selected_answer = None
         st.rerun()
+score = st.session_state.score
